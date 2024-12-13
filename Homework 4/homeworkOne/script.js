032db61be7c68e5442a627885d01dc0bab90543a
Filip Trifunovski <filiptrifunovski88@gmail.com>
2024-12-13 01:37:32 +0100

    Homework 4

---
+++ Homework 4/homeworkOne/script.js
@@ -0,0 +1,7 @@
+function tellStory(name, mood, activity) {
+  let arrayOfStrings = ["Filip", "Happy", "Reading a book"];
+  console.log(
+    `This is ${arrayOfStrings[0]}. ${arrayOfStrings[0]} is a nice person. Today he is ${arrayOfStrings[1]}.He is ${arrayOfStrings[2]} all day.`
+  );
+}
+tellStory();
