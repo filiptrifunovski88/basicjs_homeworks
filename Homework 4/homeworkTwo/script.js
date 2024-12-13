7a34a03d9917811fa3f307a70dc78106e2882d95
Filip Trifunovski <filiptrifunovski88@gmail.com>
2024-12-13 14:15:41 +0100

    Homework 4

---
+++ Homework 4/homeworkTwo/script.js
@@ -0,0 +1,12 @@
+function sumOfNumbers() {
+  let arrayNumbers = [43, 23, 11, 65, 21];
+  let sum =
+    arrayNumbers[0] +
+    arrayNumbers[1] +
+    arrayNumbers[2] +
+    arrayNumbers[3] +
+    arrayNumbers[4];
+  console.log(sum);
+  return sum;
+}
+sumOfNumbers();
