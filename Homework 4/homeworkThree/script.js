abe11cad357940487e10145ede15be59475765a0
Filip Trifunovski <filiptrifunovski88@gmail.com>
2024-12-13 14:23:27 +0100

    Homework 4

---
+++ Homework 4/homeworkThree/script.js
@@ -0,0 +1,7 @@
+function stringArray() {
+  let enterArrayStrings = ["Hello", "there", "students", "of", "SEDC", "!"];
+  console.log(
+    `"${enterArrayStrings[0]} ${enterArrayStrings[1]} ${enterArrayStrings[2]} ${enterArrayStrings[3]} ${enterArrayStrings[4]}${enterArrayStrings[5]}"`
+  );
+}
+stringArray();
